f52f66835c5233d71b2e161fdde76b8925067537
JoshDT900 <DJTroutman900@gmail.com>
2023-01-30 09:50:30 -0500

    Fix a small bug and added page content

--- src/funcs.js
+++ src/funcs.js
@@ -4,20 +4,17 @@ const domEleGen = (function() {
     const makeEle = function() {
         let argArr = [...arguments];
 
-        console.log(argArr, typeof(argArr[1]));
-
         if (typeof(argArr[0]) != 'string') {
             return alert("First argument must be a valid HTML element in string format. Example: 'div' ");
+        } else if (typeof(argArr[1]) != "string" && argArr.length === 2) {
+            return alert("Invalid Argument Type. The second argument must be a string");
+        } else if (typeof(argArr[2]) != "object" && argArr.length === 3) {
+            return alert("Invalid Arg. Third argument accepts an array or a nested array of arrays.");
         }
 
         let newEle = document.createElement(`${argArr[0]}`);
-
-        if (argArr.length >= 2 && typeof(argArr[1]) === "string"){
-            newEle.textContent = argArr[1];
-        } else {
-            return alert("Invalid Argument Type. The second argument must be a string");
-        }
-
+        newEle.textContent = argArr[1];
+        
         if (argArr.length === 3 && typeof(argArr[2][0]) === "object"){
             for (let i in argArr[2]){                
                 let tempArr = [...argArr[2][i]];
@@ -26,19 +23,13 @@ const domEleGen = (function() {
             }
         } else if (typeof(argArr[2]) === "object") {
             newEle.setAttribute(...argArr[2]);
-        } else {
-            return alert("Invalid Arg. Third argument accepts an array or a nested array of arrays.")
         }
+        
 
         return newEle;   
     }
 
     return { makeEle };
 })();
-
-let bodyEl = document.querySelector("body");
-let testEle = domEleGen.makeEle("div", "", ["class", "test"]);
-
-bodyEl.appendChild(testEle)
 
 export { domEleGen }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 import css from "./style.css"
 import { imageAssets } from "./assets.js"
+import { domEleGen } from "./funcs.js";
+
+let pageRender = () => {
+    let bodyEl = document.querySelector("body");
+
+    navRender(bodyEl);
+
+    return bodyEl;
+}
+
+let navRender = (ele) => {
+    let navEl = domEleGen.makeEle("nav", "", ["class", "main_nav"]);
+    ele.appendChild(navEl);
+
+    let pTitleEle = domEleGen.makeEle("div", "", ["class", "page_title"]);
+    navEl.appendChild(pTitleEle);
+
+    let pHoneEle = domEleGen.makeEle("h1", "Image Slider - The Odin Project", ["class", "title_text"]);
+    pTitleEle.appendChild(pHoneEle);
+
+    let gitImgWrap = domEleGen.makeEle("div", "", ["class", "git_img_wrap"]);
+    navEl.appendChild(gitImgWrap);
+
+    let gitAncAtr = [["href", "https://github.com/JoshDT900"], ["target", "_blank"]];
+    let gitImgEle = domEleGen.makeEle("a", "", gitAncAtr);
+    gitImgWrap.appendChild(gitImgEle);
+
+    let gitLogoAtr = [["src", imageAssets.gitImg], ["alt", "github logo"], ["class", "git_img"]]
+    let gitLogoEle = domEleGen.makeEle("img", "", gitLogoAtr);
+    let gitTextEle = domEleGen.makeEle("p", "Created by - David T.")
+    gitImgEle.appendChild(gitTextEle);
+    gitImgEle.appendChild(gitLogoEle);
 
 
+    return ele;
+}
+
+
+(() => {
+    pageRender();
+})();
